e5eb80f5f653b5d3f90184ba0713ea546a1a5a5b
pengbo <pengbo@hopefound.cn>
2023-05-30 17:20:16 +0800

    1

--- src/views/detail/video/index.vue
+++ src/views/detail/video/index.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>detailVideo</div>
+  <div>
+    <div class="xgplayer" ref="xgplayer"></div>
+  </div>
 </template>
 
 <script>
+import Player from "xgplayer";
+import "xgplayer/dist/index.min.css";
 export default {
   name: "detailVideo",
   data() {
-    return {};
+    return {
+      player: null,
+    };
   },
-  methods: {},
+  mounted() {
+    this.getStart();
+  },
+  methods: {
+    getStart() {
+      if (this.player != null) {
+        this.player.destroy();
+        this.player = null;
+      }
+      this.player = new Player({
+        el: this.$refs.xgplayer,
+        url: "//abc.com/**/*.mp4",
+        height: "100%",
+        width: "100%",
+      });
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
