78a8db1e81dc7e63f801ce519da11ac82cc2c00e
pengbo <pengbo@hopefound.cn>
2023-05-30 17:07:59 +0800

    1

---
+++ src/views/detail/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>detailtest</div>
+</template>
+
+<script>
+export default {
+  name: "detailTest",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/detail/video/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>detailVideo</div>
+</template>
+
+<script>
+export default {
+  name: "detailVideo",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+</style>
