9b8a84d5506614076bc0bb4a0f85a7645d668b2e
pengbo <pengbo@hopefound.cn>
2023-06-01 17:53:37 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,40 +1,52 @@
 <template>
   <div>
-    <div class="fapage">
-      <div class="login">
-        <img class="logo" src="@/assets/imgs/logo.png" alt="" />
-        <h3 class="title">后台管理系统</h3>
-        <el-input
-          class="username"
-          placeholder="请输入账号"
-          prefix-icon="el-icon-user"
-          v-model="username"
-        >
-        </el-input>
-        <el-input
-          class="password"
-          placeholder="请输入密码"
-          :show-password="true"
-          type="password"
-          prefix-icon="el-icon-lock"
-          v-model="password"
-        >
-        </el-input>
-        <el-button
-          class="btn"
-          type="primary"
-          @click="toLogin"
-          :loading="isLoading"
-          >登 录</el-button
-        >
+    <headerBox></headerBox>
+    <div class="body">
+      <div class="fbox">
+        <div class="sbox w1200">
+          <div></div>
+          <div class="loginBox">
+            <h3 class="title">后台管理系统</h3>
+            <el-input
+              class="username"
+              placeholder="请输入账号"
+              prefix-icon="el-icon-user"
+              v-model="username"
+            >
+            </el-input>
+            <el-input
+              class="password"
+              placeholder="请输入密码"
+              :show-password="true"
+              type="password"
+              prefix-icon="el-icon-lock"
+              v-model="password"
+            >
+            </el-input>
+            <el-button
+              class="btn"
+              type="primary"
+              @click="toLogin"
+              :loading="isLoading"
+              >登 录</el-button
+            >
+          </div>
+        </div>
       </div>
     </div>
+    <footerBox></footerBox>
   </div>
 </template>
 
 <script>
+import headerBox from "@/components/header";
+import footerBox from "@/components/footer";
 export default {
   name: "login",
+  components: {
+    headerBox,
+    footerBox,
+  },
   data() {
     return {
       username: "测试",
@@ -61,57 +73,58 @@ export default {
       this.isLoading = true;
       this.$unit.setLocalStorage("username", this.username);
       this.$router.push({
-        name: "adminHome",
+        name: "home",
       });
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-.fapage {
-  height: 100vh;
+.body {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  background-color: #2d3a4b;
-  .login {
-    width: 400px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: #fff;
-    .logo {
-      width: 80px;
-      height: 80px;
-      margin-bottom: 15px;
-    }
-    .title {
-      margin-bottom: 20px;
-      font-size: 26px;
-    }
-    .username {
-      margin-bottom: 10px;
-      /deep/ .el-input__inner {
-        height: 45px;
-        line-height: 45px;
-        background-color: #1f2b3a !important;
-        border-color: #1f2b3a !important;
-        color: #fff !important;
+  overflow: hidden;
+
+  .fbox {
+    height: 601px;
+    width: 1920px;
+    background-image: url("@/assets/imgs/banner-login.jpg");
+    .sbox {
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .loginBox {
+        border-radius: 20px;
+        padding: 0px 30px;
+        width: 350px;
+        height: 420px;
+        background-color: #ffffffef;
+
+        .title {
+          margin-bottom: 20px;
+          font-size: 26px;
+        }
+        .username {
+          margin-bottom: 10px;
+          /deep/ .el-input__inner {
+            height: 45px;
+            line-height: 45px;
+          }
+        }
+        .password {
+          margin-bottom: 15px;
+          /deep/ .el-input__inner {
+            height: 45px;
+            line-height: 45px;
+          }
+        }
+        .btn {
+          width: 100%;
+          margin-bottom: 50px;
+        }
       }
-    }
-    .password {
-      margin-bottom: 15px;
-      /deep/ .el-input__inner {
-        height: 45px;
-        line-height: 45px;
-        background-color: #1f2b3a !important;
-        border-color: #1f2b3a !important;
-        color: #fff !important;
-      }
-    }
-    .btn {
-      width: 100%;
-      margin-bottom: 50px;
     }
   }
 }
